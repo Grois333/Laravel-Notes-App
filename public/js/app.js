25ac1ad7f5b0ed17d9717c78f68c8d02b9894357
Grois333 <isaacgroisman@gmail.com>
2024-02-26 20:49:27 +0200

    update

--- public/js/app.js
+++ public/js/app.js
@@ -64917,8 +64917,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\isaac\OneDrive\Escritorio\laravel_inertia_vue\curso-spa-laravel-inertia-vue\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\isaac\OneDrive\Escritorio\laravel_inertia_vue\curso-spa-laravel-inertia-vue\resources\css\app.css */"./resources/css/app.css");
+__webpack_require__(/*! C:\Users\isaac\Documents\UBUNTU\Documents\Code Projects\laravel_inertia_vue\curso-spa-laravel-inertia-vue\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\isaac\Documents\UBUNTU\Documents\Code Projects\laravel_inertia_vue\curso-spa-laravel-inertia-vue\resources\css\app.css */"./resources/css/app.css");
 
 
 /***/ })
